8df818f4ef12d4430e353072f9b4b9ed2b6a10fb
eman hamid <emanhharki@gmail.com>
2021-04-27 19:40:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+
 // Your code here
+class Dog{
+constructor(name,sex){
+this.name = name;
+this.sex = sex;
+}
+speak(){
+  return this.name + "say woof!";
+}
+}
+const dog = new Dog("","female");
+console.log(dog.speak());
+class Cat{
+  constructor(name,sex){
+    this.name = name;
+    this.sex = sex;
+  }
+}
+class Bird{
+  constructor(name,sex){
+    this.name = name;
+    this.sex = sex;
+  }
+}
