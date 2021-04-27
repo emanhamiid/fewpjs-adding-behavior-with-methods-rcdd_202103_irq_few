7b10dd544ba4196bb42a7c98628f887faad8964b
eman hamid <emanhharki@gmail.com>
2021-04-27 20:01:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,5 +42,5 @@ class Bird{
 const bird1 = new Bird("Pablo","male");
 console.log(bird.speak());
 
-const bird2 = new Bird("Mable");
+const bird2 = new Bird("Mable","");
 console.log("Mable"+ " " +"says"+ " " + "squawk!");
